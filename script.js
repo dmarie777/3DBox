ca76c007eadd36f7ee00124d37b618e6f0cf20bc
dmarie777 <deimarie777@gmail.com>
2024-04-11 23:35:09 -0500

    Added animate function

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ console.log(THREE);
 //Create Scene
 const scene = new THREE.Scene();
 
-//|Create objects, we need to create a mesh which is a combination of a geometry (the shape) and the material(how it looks)
+//Create objects, we need to create a mesh which is a combination of a geometry (the shape) and the material(how it looks)
 
 const geometry =new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({color: 0xff0000})
@@ -18,13 +18,12 @@ const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
 //Add the camera, there are many kinds of cameras, and we can even have many cameras in one scene
-//well use the perspective camera that makes an object look small if its away and bigger if its closer
 //create size of scene
 const size = {
     width: 800,
     height: 600
 }
-//create camera /// usually he uses 35
+//create camera /// 35
 const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
 camera.position.z = 3
 scene.add(camera)
@@ -34,4 +33,14 @@ const renderer = new THREE.WebGLRenderer({
     canvas:canvas
 })
 renderer.setSize(size.width, size.height)
-renderer.render(scene, camera)
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.01;
+
+    renderer.render(scene, camera)
+}
+
+animate()
